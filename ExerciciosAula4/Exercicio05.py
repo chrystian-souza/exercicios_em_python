dd923aca41c8a3c4f73056f1c527a9358bf42abf
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-03-05 14:36:17 -0300

    commit 01

---
+++ ExerciciosAula4/Exercicio05.py
@@ -0,0 +1,6 @@
+# # 5 - Escreva um programa para exibir apenas os números da lista gerada no exercício 4 que
+# satisfaçam as seguintes condições:
+# - O número deve ser divisível por cinco
+# - Se o número for maior que 95 e menor que 150 e, pule-o e passe para o próximo
+# número
+# - Se o número for maior que 1500, pare o loop
