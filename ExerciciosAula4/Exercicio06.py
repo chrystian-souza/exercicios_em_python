733e7cc66adf4d9cfdaf86fd3683c1ca2e246674
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-03-10 23:15:47 -0300

    reparos

--- ExerciciosAula4/Exercicio06.py
+++ ExerciciosAula4/Exercicio06.py
@@ -1,9 +1,14 @@
 # 6 - Crie um programa que use uma iteração para exibir elementos  da
 # lista gerada no exercício 4 presentes em posições de índice ímpares:
 
+# 6 - Crie um programa que use uma iteração para exibir elementos da lista gerada no
+# exercício 4 presentes em posições de índice ímpares:
+
+
 import random
 
 lista = []
+
 contador = 0
 
 while contador < 10:
@@ -12,3 +17,15 @@ while contador < 10:
     contador += 1
 for i in range(1, len(lista), 2):
     print(f'{i} - {lista[i]}')
+
+lista_impares = []
+contador = 0
+while contador < 10:
+    n = random.randint(10, 1580)
+    lista.append(n)
+
+    for lista in range(0, 2):
+        print(lista.index(n))
+
+    contador += 1
+# print(n)
