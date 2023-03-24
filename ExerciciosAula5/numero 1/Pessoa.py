d855de4b5edd2d6c3abb9197d08acc306a95e6c7
chrystian.meyer <chrystian.souza.silveira@gmail.com>
2023-03-24 20:31:19 -0300

    primeiro commit

---
+++ ExerciciosAula5/numero 1/Pessoa.py
@@ -0,0 +1,7 @@
+class Pessoa:
+    def __init__(self, id, nome, idade):
+        self.id = id
+        self.nome = nome
+        self.idade = idade
+
+    def caminhar(self):
--- ExerciciosDoSite/Exercicio3.py
+++
@@ -1,6 +0,0 @@
-# 03 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-
-raio = float(input('Informe o raio do circulo: '))
-
-area = raio**
-print(area)
