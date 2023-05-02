90a597057a41b17da98059982105f28e135bab55
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-05-01 21:24:22 -0300

    commit1

--- exerciciosDoSite/exercicio2.py
+++ exerciciosDoSite/exercicio2.py
@@ -3,4 +3,10 @@
 metros = float(input('Informe os metros: '))
 
 centimetros = metros * 100
-print(f'{metros} metros equivale a:  {centimetros} centimetros')
+
+if metros == 1.0:
+    print(f'{metros} metro equivale a: {centimetros} centimetros')
+elif metros == 0.0:
+    print(f'{metros} metro equivale a: {centimetros} centimetro')
+else:
+    print(f'{metros} metros equivale a: {centimetros} centimetros')
